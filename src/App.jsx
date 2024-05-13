04d632d67ff7605db2c5219bfee0b478ebd3a0c8
AminSalamatin <amin.salamatin.ps4@gmail.com>
2024-05-14 01:54:48 +0300

    Technical Experience label component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import TechnicalExperience from "./components/TechnicalExperience";
 
 function App() {
   return (
     <div className="flex flex-col bg-white min-h-full">
       <Navbar />
-      <div className="flex-grow"></div>
+      <div className="flex-grow">
+        <TechnicalExperience />
+      </div>
       <Footer />
     </div>
   );
---
+++ src/components/TechnicalExperience.jsx
@@ -0,0 +1,15 @@
+const technicalExperience = () => {
+  const label = "border 2 px-2 mx-2";
+
+  return (
+    <>
+      <h1 className="text-left pb-20">Technical Experience</h1>
+      <ul className="flex">
+        <li className={label}>Java</li>
+        <li className={label}>JavaScript</li>
+      </ul>
+    </>
+  );
+};
+
+export default technicalExperience;
