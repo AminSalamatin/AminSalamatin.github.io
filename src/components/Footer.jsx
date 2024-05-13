cce4d357aa6bfe28d65a94d8e4f6eaff134da77e
AminSalamatin <amin.salamatin.ps4@gmail.com>
2024-05-14 01:40:56 +0300

    Approximate align for nav and footer elements

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,9 @@
 const Footer = () => {
     return(
-        <footer className="bg-gray-800 text-white bottom-0 left-0 w-full p-10">
-            THE FOOTER
+        <footer className="bg-gray-800 text-white bottom-0 left-0 p-10 flex justify-between">
+            <p>Contact info</p>
+            <p>About me(maybe)</p>
+            <p>Something interesting...</p>
         </footer>
     )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,13 @@
 const Navbar = () => {
     return(
-        <nav className="sticky top-0 w-full bg-white z-10 shadow">
-            THE NAVBAR
+        <nav className="sticky top-0 w-full p-10 bg-gray-800 text-white z-10 flex justify-between">
+            <p>Name</p>
+            <ul className="flex">
+                <list className="px-10"><p>item1</p></list>
+                <list className="px-10"><p>item2</p></list>
+                <list className="px-10"><p>item3</p></list>
+                <list className="px-10"><p>item4</p></list>
+            </ul>
         </nav>
     )
 }
