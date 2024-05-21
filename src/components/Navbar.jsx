737c5ed3f33d6b539d8e56db54568bcc97c3f554
AminSalamatin <amin.salamatin.ps4@gmail.com>
2024-05-21 12:57:25 +0300

    CSS changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,25 @@
 const Navbar = () => {
   return (
     <nav className="bg-secondary sticky top-0 w-full p-10 z-10 border-b border-text-shadow ">
-      <div className="max-w-[50vw] flex justify-between mx-auto">
-        <h3 className="mb-0">Amin Salamatin</h3>
-        <ul className="flex">
-          <list>
+      <div className="max-w-[70vw] flex justify-between mx-auto">
+        <h1 className="mb-0">Amin Salamatin</h1>
+        <ul className="flex my-auto">
+          <li>
             <a
               href=""
               className="px-4 ml-5 text-accent border-4 border-accent text-3xl rounded-md hover:border-accent-light hover:text-accent-light hover:bg-dominant-light cursor-pointer"
             >
               LinkedIn
             </a>
-          </list>
-          <list>
+          </li>
+          <li>
             <a
               href=""
               className="px-4 ml-5 text-accent border-4 border-accent text-3xl rounded-md hover:border-accent-light hover:text-accent-light hover:bg-dominant-light cursor-pointer"
             >
               Resume
             </a>
-          </list>
+          </li>
         </ul>
       </div>
     </nav>
