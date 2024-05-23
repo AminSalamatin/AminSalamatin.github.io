218f19264c34b753ca96df32d38195fe2d8ef294
AminSalamatin <amin.salamatin.ps4@gmail.com>
2024-05-23 22:47:44 +0300

    Added time

--- src/App.jsx
+++ src/App.jsx
@@ -20,11 +20,11 @@ function App() {
           pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
           culpa qui officia deserunt mollit anim id est laborum.
         </p>
-        <Portfolio />
-        <Skills />
-        <Education />
+        <Skills/>
+        <Portfolio/>
+        <Education/>
       </div>
-      <Footer />
+      <Footer/>
     </div>
   );
 }
