490914ffa6ed23f077f20581568bc4dd9fd8e76c
AminSalamatin <115285429+AminSalamatin@users.noreply.github.com>
2024-10-04 00:49:35 +0300

    Update Skills.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,7 +2,7 @@ const Skills = () => {
   return (
     <div className="mb-20">
       <h1 className="text-left">Technical Experience</h1>
-      <ul className="flex text-2xl font-bold text-accent border-accent flex-wrap">
+      <ul className="flex text-2xl font-bold text-accent border-accent flex-wrap justify-between">
         <li className="skill">Java</li>
         <li className="skill">JavaScript</li>
         <li className="skill">TypeScript</li>
@@ -17,6 +17,7 @@ const Skills = () => {
         <li className="skill">GraphQL</li>
         <li className="skill">Microsoft Azure</li>
         <li className="skill">Unit Testing</li>
+        <li className="skill">Tailwind CSS</li>
         <li className="skill">AJAX</li>
       </ul>
     </div>
