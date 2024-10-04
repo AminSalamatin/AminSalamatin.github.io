d884fe3a4ace08b7a5a46be338edbc4d13cb7837
AminSalamatin <115285429+AminSalamatin@users.noreply.github.com>
2024-10-04 05:25:15 +0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
-    <footer className="bg-secondary bottom-0 left-0 p-10 border-t border-text-shadow">
-      <h5>Contact info</h5>
+    <footer className="bg-secondary bottom-0 left-0 p-10 border-t border-text-shadow text-2xl">
+      <h4>Contact info</h4>
       <p>Email: salamatinamin@gmail.com</p>
       <p>Phone number: +358 40 2213245</p>
     </footer>
