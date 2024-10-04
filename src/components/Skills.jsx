55808dced0daa7be6e266d00468761bd6262afcf
AminSalamatin <115285429+AminSalamatin@users.noreply.github.com>
2024-10-04 03:17:20 +0300

    Update Skills.jsx spacing

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,24 +2,25 @@ const Skills = () => {
   return (
     <div className="mb-20">
       <h1 className="text-left">Technical Experience</h1>
-      <ul className="flex text-2xl font-bold text-accent border-accent flex-wrap">
+      <ul className="flex text-2xl font-bold text-accent border-accent flex-wrap justify-between">
         <li className="skill">Java</li>
         <li className="skill">JavaScript</li>
         <li className="skill">TypeScript</li>
-        <li className="skill">Node.js</li>
-        <li className="skill">MongoDB</li>
         <li className="skill">React</li>
-        <li className="skill">MongoDB</li>
+        <li className="skill">REST API</li>
         <li className="skill">MySql</li>
+        <li className="skill">AJAX</li>
+        <li className="skill">Node.js</li>
         <li className="skill">Python</li>
         <li className="skill">Docker</li>
-        <li className="skill">REST API</li>
+        <li className="skill">MongoDB</li>
         <li className="skill">GraphQL</li>
+        <li className="skill">Tailwind CSS</li>
         <li className="skill">Microsoft Azure</li>
         <li className="skill">Unit Testing</li>
-        <li className="skill">Tailwind CSS</li>
-        <li className="skill">AJAX</li>
-      </ul>
+        <li className="skill">Git</li>
+        <li className="flex-grow"></li>
+      </ul> 
     </div>
   );
 };
